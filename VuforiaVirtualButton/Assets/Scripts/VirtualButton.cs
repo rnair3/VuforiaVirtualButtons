0ffeb080b119af61cb354463f87d1f6b600c0af4
Rinita Nair <rnair3@uncc.edu>
2019-09-23 22:14:25 -0400

    Underestanding Virtual Buttons

---
+++ VuforiaVirtualButton/Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler
+{
+    [SerializeField] GameObject sphere;
+    [SerializeField] GameObject cube;
+
+    VirtualButtonBehaviour vrb;
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        sphere.SetActive(true);
+        cube.SetActive(false);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cube.SetActive(true);
+        sphere.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vrb = GetComponentInChildren<VirtualButtonBehaviour>();
+        vrb.RegisterEventHandler(this);
+
+        cube.SetActive(true);
+        sphere.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
